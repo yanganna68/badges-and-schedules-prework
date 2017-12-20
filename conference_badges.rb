4a7399c01ee47b4502f345a9d2c4929bc90a88f6
ANNA YANG <yanganna68@gmail.com>
2017-12-20 21:19:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+  attendees.map.with_index {|name,index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each { |line| puts line}
+  assign_rooms(attendees).each { |line| puts line}
+end
